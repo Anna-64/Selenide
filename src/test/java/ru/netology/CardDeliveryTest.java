b8ca79f7e42fc028de407af61f9b9517e89c81d7
Anna-64 <shurygina6410@gmail.com>
2022-03-27 17:42:32 +0400

    added CardDeliveryTest.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,7 +4,7 @@ package ru.netology;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
+
 
 
 import java.time.LocalDate;
@@ -15,14 +15,14 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static java.time.Duration.ofSeconds;
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static org.openqa.selenium.Keys.chord;
+
 
 public class CardDeliveryTest {
 
     @BeforeEach
     public void setUp() {
 //        Configuration.holdBrowserOpen = true;
-        Selenide.open("http://localhost:9999");
+        Selenide.open("http://localhost:9999/");
     }
 
     String generateDate(int days) {
@@ -31,11 +31,10 @@ public class CardDeliveryTest {
     }
 
     @Test
-    public void shouldSendForm() {
-        String planningDate = generateDate(30);
+    void shouldSendForm() {
+        String planningDate = generateDate(3);
         $("[data-test-id='city'] input").val("Волгоград");
-//        $("[data-test-id='date'] input").val("30.03.2022");
-        $("[placeholder='Дата встречи']").sendKeys(chord(Keys.SHIFT, Keys.HOME), Keys.DELETE, planningDate);
+//      $("[placeholder='Дата встречи']").sendKeys(chord(Keys.SHIFT, Keys.HOME), Keys.DELETE, planningDate);
         $("[data-test-id='name'] input").val("Абдуллаев Ян");
         $("[data-test-id='phone'] input").val("+79005008090");
         $("[data-test-id=agreement]>.checkbox__box").click();
